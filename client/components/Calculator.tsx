c7316f6b0acea1cf3687a9ab62487cff0f6d7ea6
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-12-21 17:10:29 +0800

    flex styling in progress

--- client/components/Calculator.tsx
+++ client/components/Calculator.tsx
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css';
+import calculatorButtonInfo from './calculatorButtons.json'
 
 
 export default function Calculator(){
@@ -6,11 +7,30 @@ export default function Calculator(){
         <div className={styles.calculator}>
           <h1>Cal_cpp_ulator</h1>
           <div>
-            <div className={styles.calculatorScreen}>
-                
+            <div className={styles.calculatorScreen}>              
             </div>
             <div className={styles.calculatorKeypad}>
-
+              {calculatorButtonInfo.rowOne.map((calcBtnSymbol, index) => 
+                    <div key={index} className={styles.calcBtn}>{calcBtnSymbol}</div>
+                  )}
+                {calculatorButtonInfo.rowTwo.map((calcBtnSymbol, index) => 
+                    <div key={index} className={styles.calcBtn}>{calcBtnSymbol}</div>
+                  )}                
+                {calculatorButtonInfo.rowThree.map((calcBtnSymbol, index) => 
+                    <div key={index} className={styles.calcBtn}>{calcBtnSymbol}</div>
+                  )}                
+                {calculatorButtonInfo.rowFour.map((calcBtnSymbol, index) => 
+                    <div key={index} className={styles.calcBtn}>{calcBtnSymbol}</div>
+                  )}                
+                  <div>
+                    {calculatorButtonInfo.rowFive[1]}
+                  </div>
+                  <div>
+                    {calculatorButtonInfo.rowFive[2]}
+                  </div>
+                  <div>
+                    {calculatorButtonInfo.rowFive[3]}
+                  </div>
             </div>
           </div>
         </div>
