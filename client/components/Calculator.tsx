7e1481cce8eaf0a0b8f6d1d9c47898fbe88c4841
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-12-21 09:47:13 +0800

    add components in progress

---
+++ client/components/Calculator.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css';
+
+
+export default function Calculator(){
+    return (
+        <div className={styles.calculator}>
+          <h1>Cal_cpp_ulator</h1>
+          <div>
+            <div className={styles.calculatorScreen}>
+                
+            </div>
+            <div className={styles.calculatorKeypad}>
+
+            </div>
+          </div>
+        </div>
+    )
+}
---
+++ client/components/Footer.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(faGithub, faLinkedin, faEnvelope);
+
+export default function Footer(){
+    return (
+        <div className={styles.footer}>
+            <p>Learn more about the creator</p>
+            <a title="Github" href="https://github.com/rpet064"><FontAwesomeIcon icon={faGithub} /></a>
+            <a title="Linkedin" href="https://www.linkedin.com/in/robert-pether-ba9968113"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a title="Email" href="mailto:rpether@hotmail.co.nz"><FontAwesomeIcon icon={faEnvelope} /></a>
+            <p>Copyright Robert Pether {new Date().getFullYear()}</p>
+        </div>
+    )
+}
