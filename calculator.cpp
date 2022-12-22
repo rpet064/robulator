4304987d4106e33a0bb85cdb1a41af51419fd4bb
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-12-22 12:16:15 +0800

    calculator useState tracking user inputs complete

--- calculator.cpp
+++ calculator.cpp
@@ -1,35 +1,78 @@
 #include <iostream>
+#include <string> 
 using namespace std;
 
-int addNum(int first_num, int second_num){
+int addNum(int first_num, int second_num)
+{
     return first_num + second_num;
 }
 
-// int subtractNum(){
-    
-// }
+int subtractNum(int first_num, int second_num)
+{
+        return first_num - second_num;
+}
 
-// int multiplyNum(){
-    
-// }
+int multiplyNum(int first_num, int second_num)
+{
+        return first_num * second_num;
+}
 
-// int divideNum(){
-    
-// }
+int divideNum(int first_num, int second_num)
+{
+    return first_num / second_num;
+}
+
+void calculateAnswer(string operation, int first_num, int second_num)
+{
+    int answer{};
+    if (operation == "+" )
+    {
+        answer = addNum(first_num, second_num);
+    } 
+    else if (operation == "-" )
+    {
+        answer = subtractNum(first_num, second_num);
+    }
+    else if (operation == "*" )
+    {
+        answer = multiplyNum(first_num, second_num);
+    }
+    else if (operation == "/" )
+    {
+        answer = divideNum(first_num, second_num);
+    } else {
+
+        // end function if user inputs unsupported operator 
+        std::cout << "Sorry, at this stage only +, -, / or * can be used \n";
+        return;
+    }
+    std::cout << first_num << operation << second_num << "=" << answer << "\n";
+}
+
+// gets numeric value from user and returns to main
+int getNumValue()
+{
+    int userInput {};
+    std::cout << "Please enter a number" << "\n";
+    std::cin >> userInput;
+    return userInput;
+}
 
 int main()
 {
-    int first_num {}, second_num{};
-    string operation {};
-
-    std::cout << "Welcome to the calculator. What's your first number? \n";
-    std::cin >> first_num;
+    int first_num {};
+        first_num = getNumValue();
+    string operation = "";
     std::cout << "What would you like to do with it? \n";
-    std::cin >> operation;
-    std::cout << "What's your second number? \n";
-    std::cin >> second_num;
-    int answer{addNum(first_num, second_num)};
-    std::cout << "Answer is " << answer << "\n";
+    std::cin >> operation;    
+    int second_num {};
+    second_num = getNumValue();
+    calculateAnswer(operation, first_num, second_num);
 
+    // catch if user puts in letter instead of number
+    if (!std::cin.good())
+    {
+        throw std::invalid_argument( "Why did you put in a letter?" );
+    }
     return 0;
 }
--- client/components/Calculator.tsx
+++ client/components/Calculator.tsx
@@ -1,35 +1,47 @@
 import styles from '../styles/Home.module.css';
-import calculatorButtonInfo from './calculatorButtons.json'
-
+import calculatorButtonInfo from './calculatorButtons.json';
+import { useState } from 'react';
 
 export default function Calculator(){
+  const [calculatorInput, setCalculatorInput] = useState<string[]>([])
+  
+  const onChangeHandler = (e: any) => {
+    if (e.target.value === 'AC'){
+      setCalculatorInput([])
+    } else if ((e.target.value === '=')) {
+      alert('submitted')
+      setCalculatorInput([])
+    } else {
+      setCalculatorInput(calculatorInput => [...calculatorInput, e.target.value]);
+    }
+  }
+
     return (
         <div className={styles.calculator}>
-          <h3>Hello Calculator</h3>
           <div>
             <div className={styles.calculatorScreen}>     
-              <span>50 + 10 = 60</span>        
+              <span>{calculatorInput}</span>        
             </div>
             <div className={styles.calculatorKeypad}>
               {calculatorButtonInfo.rowOne.map((calcBtnSymbol, index) => 
-                    <button key={index} className={styles.calcBtn}>{calcBtnSymbol}</button>
+                    <button onClick={onChangeHandler} value={calcBtnSymbol} key={index} className={styles.calcBtn}>{calcBtnSymbol}</button>
                   )}
                 {calculatorButtonInfo.rowTwo.map((calcBtnSymbol, index) => 
-                    <button key={index} className={styles.calcBtn}>{calcBtnSymbol}</button>
+                    <button onClick={onChangeHandler} value={calcBtnSymbol} key={index} className={styles.calcBtn}>{calcBtnSymbol}</button>
                   )}                
                 {calculatorButtonInfo.rowThree.map((calcBtnSymbol, index) => 
-                    <button key={index} className={styles.calcBtn}>{calcBtnSymbol}</button>
+                    <button onClick={onChangeHandler} value={calcBtnSymbol} key={index} className={styles.calcBtn}>{calcBtnSymbol}</button>
                   )}                
                 {calculatorButtonInfo.rowFour.map((calcBtnSymbol, index) => 
-                    <button key={index} className={styles.calcBtn}>{calcBtnSymbol}</button>
+                    <button onClick={onChangeHandler} value={calcBtnSymbol} key={index} className={styles.calcBtn}>{calcBtnSymbol}</button>
                   )}                
-                  <button className={styles.calcBtnZero}>
+                  <button onClick={onChangeHandler} value={calculatorButtonInfo.rowFive[0]} className={styles.calcBtnZero}>
                     <span>{calculatorButtonInfo.rowFive[0]}</span>
                   </button>
-                  <button className={styles.calcBtn}>
+                  <button onClick={onChangeHandler} value={calculatorButtonInfo.rowFive[1]} className={styles.calcBtn}>
                     {calculatorButtonInfo.rowFive[1]}
                   </button>
-                  <button className={styles.calcBtn}>
+                  <button onClick={onChangeHandler} value={calculatorButtonInfo.rowFive[2]} className={styles.calcBtn}>
                     {calculatorButtonInfo.rowFive[2]}
                   </button>
             </div>
