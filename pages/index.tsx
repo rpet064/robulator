9be56d75334bab48fb46be4b58b01234110ab2ce
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-12-25 21:28:32 +0800

    edit favicon for pinned websites

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           Calculator Robert Pether Junior Front-End Developer Ios "/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="/images/favicon.ico" sizes="any"/>
-        <link rel="apple-touch-icon" sizes="192x192" href="/images/android-chrome-192x192" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" color="white"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
       </Head>
