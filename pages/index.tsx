6a9514d05843dc1f9f44daacad827975007095d7
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-12-25 21:18:29 +0800

    edit favicon for pinned websites

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,11 +17,10 @@ export default function Home() {
           name="keywords" content="React Next Node Cpp 
           Calculator Robert Pether Junior Front-End Developer Ios "/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" sizes="any"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
-        <link rel="mask-icon" href="/images/favicon-32x32.png" color="white"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
       </Head>
       <main className={styles.main}>
         <Calculator />
