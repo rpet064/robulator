01ee5285a9ef6f233830856ce6b447bdb17b15f8
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-01-04 21:33:51 +1300

    move toast message into seprate folder

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react"
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faAngleLeft, faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAngleLeft, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from '../styles/Home.module.css'
-import toast from 'react-hot-toast';
+import { notifyMessage } from "./utility/toastMessages"
 
-library.add(faAngleLeft, faAngleDown);
+library.add(faAngleLeft, faAngleDown)
 
 export default function SideMenu() {
 
     const [isMenuCollapsed, setIsMenuCollapsed] = useState(false)
 
-    const sideNavBarToggleIcon = isMenuCollapsed ? faAngleDown : faAngleLeft;
+    const sideNavBarToggleIcon = isMenuCollapsed ? faAngleDown : faAngleLeft
 
     const toggleMenu = () => {
         setIsMenuCollapsed(!isMenuCollapsed)
 
-        toast('In progress');
+        notifyMessage('In progress')
     }
 
     return(
