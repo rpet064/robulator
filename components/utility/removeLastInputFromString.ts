b8877863c6dcd47dba6c6cbd6f3d375f8033b018
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-02-06 22:14:10 +1300

    Update removeLastInputFromString.ts to remove pi
    Update removeLastInputFromString.ts to remove pi

--- components/utility/removeLastInputFromString.ts
+++ components/utility/removeLastInputFromString.ts
@@ -1,4 +1,12 @@
 export const removeLastInputFromString = (calculatorInput: string[]) => {
-        let arrayIntoString = calculatorInput.join("").slice(0, -1)
-        return arrayIntoString.split("")
+        let arrayIntoString = calculatorInput.join("")
+        let newArray = ""
+
+        // Remove Pi from equation
+        if (arrayIntoString.endsWith("𝝅")) {
+                newArray = arrayIntoString.replace(/𝝅$/, '');
+        } else {
+                newArray = arrayIntoString.slice(0, -1)
+        }
+        return newArray.split("")
 }
