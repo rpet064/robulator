44a8799c4078170023667a4310b34b9d1bca7901
rpet064 <93119631+rpet064@users.noreply.github.com>
2024-03-02 15:15:48 +0800

    Implement placeholder localStorageManager
    Implement placeholder localStorageManager

---
+++ components/utility/localStorageManager.ts
@@ -0,0 +1,10 @@
+export const saveCalculationToLocalStorage = (calculationToSave: string): boolean => {
+    return true
+}
+
+export const loadCalculationsFromLocalStorage = (): string[] => {
+    return []
+}
+
+export const clearCalculationsFromLocalStorage = (): void => {
+}
